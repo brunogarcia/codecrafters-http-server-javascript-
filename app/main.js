7b0d10897b29351e902c20e81b629034f91dad94
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-10 19:04:16 +0100

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,6 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  //  ensure that your server is listening for connections on the correct port.
+  socket.on("data", (data) => {
+    socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+
+    // const message = data.toString();
+    // if (message === "Hello") {
+    //   socket.write("Goodbye!");
+    // }
+  });
+
   socket.on("close", () => {
     socket.end();
     server.close();
