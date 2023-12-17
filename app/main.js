e9c19acffc1659f5ae8bd4c3a4805b9d439c7cb8
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-17 18:14:10 +0100

    pass stage

--- app/main.js
+++ app/main.js
@@ -7,17 +7,23 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   //  ensure that your server is listening for connections on the correct port.
   socket.on("data", (data) => {
+    // Step 1: Extract the path
     const message = data.toString();
-    /*
-      GET /index.html HTTP/1.1
-      Host: localhost:4221
-      User-Agent: curl/8.4.0
-      Accept:* 
-    */
     const [method, path] = message.split(" ");
+    const [_, echo, ...tail] = path.split("/");
 
     if (method === "GET" && path === "/") {
       socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+    } else if (method === "GET" && echo === "echo" && tail.length > 0) {
+      // Step 2: Create your response
+      const randomString = tail.join("/");
+      const statusLine = "HTTP/1.1 200 OK\r\n";
+      const headers = `Content-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n`;
+      const responseBody = randomString;
+      const response = statusLine + headers + responseBody;
+
+      // Step 3: Send the response
+      socket.write(response);
     } else {
       socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
     }
