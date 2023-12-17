334c3fb0fdea525315bf6393936aa0c149f75509
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-17 17:10:19 +0100

    pass stage

--- app/main.js
+++ app/main.js
@@ -7,12 +7,20 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   //  ensure that your server is listening for connections on the correct port.
   socket.on("data", (data) => {
-    socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+    const message = data.toString();
+    /*
+      GET /index.html HTTP/1.1
+      Host: localhost:4221
+      User-Agent: curl/8.4.0
+      Accept:* 
+    */
+    const [method, path] = message.split(" ");
 
-    // const message = data.toString();
-    // if (message === "Hello") {
-    //   socket.write("Goodbye!");
-    // }
+    if (method === "GET" && path === "/") {
+      socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+    } else {
+      socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+    }
   });
 
   socket.on("close", () => {
